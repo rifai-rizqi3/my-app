e0e8863af2455b195012505a487aaaaf12193065
RIZQI NUR RIFA'I <rifai.rizqi3@gmail.com>
2023-06-08 19:31:53 +0700

    update

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,7 @@ export const Home = () => {
                     <div className="wrap-text">
                         <span className="tagline">Hello World! </span>
                         <h1>Hallo, I’m Rizqi.</h1>
-                        <p> I am passionate about becoming a Fullstack developer and DevOps professional who can build user-friendly websites and add value to others by sharing knowledge and insights. By combining my expertise in frontend development and DevOps, I am dedicated to creating exceptional user experiences and ensuring smooth-running systems.
+                        <p> I am passionate about becoming a Fullstack Developer and DevOps Engineer professional who can build user-friendly websites and add value to others by sharing knowledge and insights. By combining my expertise in frontend development and DevOps, I am dedicated to creating exceptional user experiences and ensuring smooth-running systems.
                         </p>
                         <p>
                         Furthermore, I strongly believe in empowering others through the sharing of knowledge and perspectives. I take pleasure in making positive contributions to the developer community by organizing events, teaching, or contributing to open-source projects. I believe that the more we share, the more we grow together.
