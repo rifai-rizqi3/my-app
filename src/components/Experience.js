92d9e57cbc9dfc93052a98dc348414ed38914dce
RIZQI NUR RIFA'I <rifai.rizqi3@gmail.com>
2023-06-09 14:18:04 +0700

    update

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -2,11 +2,18 @@ import { Container } from "react-bootstrap";
 
 export const Experience = () => {
   return (
-    <section>
+    <section className="experience">
       <Container>
         <div className="d-flex">
           <div className="wrap-text">
-            <span className="tagline">test</span>
+            <h1>My Experience</h1>
+            <p>
+              There have been several activities that I have been through, and
+              all of them have been invaluable.
+            </p>
+            <div>
+              <i className="bi bi-1-square-fill"></i>
+            </div>
           </div>
         </div>
       </Container>
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import "mdb-react-ui-kit/dist/css/mdb.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'remixicon/fonts/remixicon.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "remixicon/fonts/remixicon.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { FaBeer } from "@react-icons/all-files/fa/FaBeer";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
